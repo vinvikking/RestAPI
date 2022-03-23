4423ab9a30c3533b1c0e1421794b0f38b4421f2c
Vincent Roeland <vincentroeland@MacBook-Pro-van-Vincent.local>
2022-03-23 18:15:21 +0100

    json datatable

--- app/Http/Controllers/CamerasController.php
+++ app/Http/Controllers/CamerasController.php
@@ -14,6 +14,14 @@ class CamerasController extends Controller
      */
     public function index()
     {
+        //echo $this->jsonToObject();
+        $test = $this->jsonToObject();
+
+        $json_decoded = json_decode($test, true);
+         print_r($json_decoded);
+        foreach($json_decoded as $result){
+         //print_r($result);
+        }
         return view('cameras.index');
     }
 
@@ -82,4 +90,35 @@ class CamerasController extends Controller
     {
         //
     }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cameras  $cameras
+     * @return \Illuminate\Http\Response
+     */
+    public function jsonToObject()
+    {
+               $curl = curl_init();
+
+              curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://api.sports.studioautomated.com/api/v3/core/cameras',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'GET',
+              CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWQxZWQ2ZjE2NGFmODM0NjY5YWZmMSIsImVtYWlsQWRkcmVzcyI6ImdvcmRvbi5nb3Nld2lzY2hAc3BvcnRjbHVic3VwcG9ydC5jb20iLCJjdXN0b21lcl9pZCI6IioiLCJwYXJ0bmVyX2lkIjoiZjQzMjVmZDQtNjkxNi00NzhmLTlkYmEtOTZkMDQ1MjlhODYxIiwicm9sZSI6InBhcnRuZXJfYWRtaW4iLCJpYXQiOjE2NDc5ODAyMDcsImV4cCI6MTY0ODU4NTAwN30.ixDMXdVWcFw7NuP4BhzYxnbo5U9tEiB24EyCixIVaxKPBScQ6gnNrR04USkpWghKix2EEPzKXhFiix3HX3a5VOllBJ5jx4WvWWfHZvNw2Of32gFAMWIMk24D0KpRN7ngFbqxdFqv2V0mMRw2OjIKmky3e9i5ZHECxd4fdgVkQZOiUAr2pSky6JFly2RsjW9AfEZOMMaMLJheGPKwtvfor01zfn7PyuYuq5EzaxQ4cz55jFJSCG9nUQ9G1X3UPRzdagK7VlPZAEJt1jk0RH0EpEWT_DhdAlxdZnqI1ovEUfXJb562l_oaWKKW9-oti_r1mLiPBi319CU_o4dKVkS7gg'
+              ),
+            ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+            }
 }
--- resources/views/cameras/index.blade.php
+++ resources/views/cameras/index.blade.php
@@ -13,29 +13,67 @@
     <p>teststeen</p>
 
 
+  <div class="container">
+    <div class="card">
+    <div class="card-body">
+    <h1 class="text text-center text-center ">
+      GeeksforGeeks
+    </h1>
+    <h6 class="text text-success text-center">
+      A computer science portal for geeks
+    </h6>
+      <table class="table-striped border-success">
+        <thead>
+          <tr>
+            <th data-field="id">
+              <span class="text-success">
+                 ID
+              </span>
+            </th>
+            <th data-field="camera_type">
+              <span class="text-success">
+                camera_types 
+              </span>
+            </th>
+            <th data-field="serial_number">
+              <span class="text-success">
+                serial_number  
+              </span>
+            </th>
+          </tr>
+        </thead>
+      </table>
+        </div>
+      </div>
+  </div>
+  
+   <script src=
+"https://code.jquery.com/jquery-3.3.1.min.js">
+  </script>
+  <script src=
+"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+  </script>
+  <script src=
+"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+  </script>
 
-<?php 
-	
-$curl = curl_init();
+ <!-- Include the JavaScript file
+  for Bootstrap table -->
+  <script src=
+"https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.js">
+  </script>
+  <script type="text/javascript">
+    $(document).ready(function () {
+  
+      // Use the given data to create 
+      // the table and display it
+      $('table').bootstrapTable({
+        data: mydata
+      });
+    });
+    console.log(mydata);
+    // Specify the JSON data to be displayed
+    var mydata = "";
+  </script>
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.sports.studioautomated.com/api/v3/core/cameras',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWQxZWQ2ZjE2NGFmODM0NjY5YWZmMSIsImVtYWlsQWRkcmVzcyI6ImdvcmRvbi5nb3Nld2lzY2hAc3BvcnRjbHVic3VwcG9ydC5jb20iLCJjdXN0b21lcl9pZCI6IioiLCJwYXJ0bmVyX2lkIjoiZjQzMjVmZDQtNjkxNi00NzhmLTlkYmEtOTZkMDQ1MjlhODYxIiwicm9sZSI6InBhcnRuZXJfYWRtaW4iLCJpYXQiOjE2NDc5ODAyMDcsImV4cCI6MTY0ODU4NTAwN30.ixDMXdVWcFw7NuP4BhzYxnbo5U9tEiB24EyCixIVaxKPBScQ6gnNrR04USkpWghKix2EEPzKXhFiix3HX3a5VOllBJ5jx4WvWWfHZvNw2Of32gFAMWIMk24D0KpRN7ngFbqxdFqv2V0mMRw2OjIKmky3e9i5ZHECxd4fdgVkQZOiUAr2pSky6JFly2RsjW9AfEZOMMaMLJheGPKwtvfor01zfn7PyuYuq5EzaxQ4cz55jFJSCG9nUQ9G1X3UPRzdagK7VlPZAEJt1jk0RH0EpEWT_DhdAlxdZnqI1ovEUfXJb562l_oaWKKW9-oti_r1mLiPBi319CU_o4dKVkS7gg'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
-
-?> 
 @endsection
